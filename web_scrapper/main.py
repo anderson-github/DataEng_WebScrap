4c9517ba09f6e933aafab5599a38e9d335c8b878
Anderson RS <ander.irs@gmail.com>
2020-04-09 19:54:16 -0300

    Pandas data selection and wrangling. newspaper_receipe.py for automating data cleaning and data wrangling.

--- web_scrapper/main.py
+++ web_scrapper/main.py
@@ -27,7 +27,7 @@ def _news_scraper(news_site_uid):
         if article:
             logger.info('Article fetched !!!')
             articles.append(article)
-            break
+            # break
 
     _save_articles(news_site_uid, articles)
 
